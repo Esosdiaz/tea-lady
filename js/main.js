edba9878388fd02de0a492a80e749704889d20bd
Fa Diaz <diazgonzalez.perla@gmail.com>
2017-05-21 21:14:27 -0400

    validaciones de texto funcionando

--- js/main.js
+++ js/main.js
@@ -1,4 +1,43 @@
-var nombre = document.getElementById("name").value;
+var validarNombre = function(nombre){
+if (nombre == ""){
+alert("Ingrese su nombre");
+}
+};
+
+var validarApellido = function(apellido){
+if (apellido == ""){
+alert("Ingrese su apellido");
+}
+};
+
+var validarCorreo = function(correo){
+if (correo == ""){
+alert("Ingrese su correo electrónico");
+}
+};
+
+var validarDireccion = function(direccion){
+if (direccion == ""){
+alert("Ingrese su dirección");
+}
+};
+
+var validar = function(){
+ var nombre = document.getElementById("name").value;
+ var apellido = document.getElementById("lastName").value;
+ var correo = document.getElementById("mail").value;
+ var direccion = document.getElementById("adress").value;
+ var telefono = document.getElementById("phone").value;
+ 
+validarNombre(nombre);
+validarApellido(apellido);
+validarCorreo(correo);
+validarDireccion(telefono);
+};
+
+
+
+/*var nombre = document.getElementById("name").value;
 var apellido = document.getElementById("lastName").value;
 var correo = document.getElementById("mail").value;
 var direccion = document.getElementById("adress").value;
@@ -27,10 +66,11 @@ var validarDireccion = function(){
 		alert("Ingrese su dirección");
 	}
 };
+
 var validar = function(){
 	validarNombre();
 	validarApellido();
 	validarCorreo();
 	validarDireccion();
 };
-
+*/
