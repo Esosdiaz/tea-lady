6517e04fb3a6b870215886876d507980803f419b
Fa Diaz <diazgonzalez.perla@gmail.com>
2017-05-21 23:43:43 -0400

    funcionalidad terminada

--- js/main.js
+++ js/main.js
@@ -13,6 +13,36 @@ if (nombre == "" || apellido == "" || correo == "" || direccion == "" || telefon
 	alert ("Por favor, rellene todos los campos");
 } else if (caja.checked === false){
 	alert ("Debe suscribirse a nuestro newsletter")
+} else {
+	var nombreDiv = document.createElement("div")
+	var nombreInput = document.createTextNode("Nombre: " + nombre)
+	nombreDiv.appendChild(nombreInput);
+	document.getElementById("clientes").appendChild(nombreDiv);
+
+	var apellidoDiv = document.createElement("div");
+	var apellidoInput = document.createTextNode("Apellido: " + apellido);
+	apellidoDiv.appendChild(apellidoInput);
+	document.getElementById("clientes").appendChild(apellidoDiv);
+
+	var correoDiv = document.createElement("div");
+	var correoInput = document.createTextNode("Correo Electrónico: " + correo);
+	correoDiv.appendChild(correoInput);
+	document.getElementById("clientes").appendChild(correoDiv);
+
+	var direccionDiv = document.createElement("div");
+	var direccionInput = document.createTextNode("Direccion: " + direccion);
+	direccionDiv.appendChild(direccionInput);
+	document.getElementById("clientes").appendChild(direccionDiv);
+
+	var telefonoDiv = document.createElement("div");
+	var telefonoInput = document.createTextNode("Número Telefónico: " + telefono);
+	telefonoDiv.appendChild(telefonoInput);
+	document.getElementById("clientes").appendChild(telefonoDiv);
+
+	var opcionDiv = document.createElement("div");
+	var opcionInput = document.createTextNode(opcion.text)
+	opcionDiv.appendChild(opcionInput);
+	document.getElementById("clientes").appendChild(opcionDiv);
 }
 }
 
